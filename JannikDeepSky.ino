af2ed698b8e2baada9711ae1f84a84fc63ff181c
Mattis Wacker <mattis.wacker@gmx.de>
2021-12-09 09:03:14 +0100

    Added comments to make the code more readable

--- JannikDeepSky.ino
+++ JannikDeepSky.ino
@@ -1,40 +1,24 @@
 /*
-Copyright (c) 2021 Jannik Meyer, Lorenz Meyer, Mattis Wacker
+ * Copyright (c) 2021 Jannik Meyer, Lorenz Meyer, Mattis Wacker
+ */
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-**/
-
-// Einbinden von Biblitheken zur anzeige auf dem LCD Display
-#include <Wire.h>
+// Include necessary libraries to communicate with the display
 #include <LiquidCrystal_I2C.h>
 
-#define PULS_OUT_PIN      (9)
-#define DIRECTION_OUT_PIN (6)
-#define SPEED_POTI_IN_PIN  A1
+// Pins
+#define PULS_OUT_PIN      9
+#define DIRECTION_OUT_PIN 6
+#define SPEED_POTI_IN_PIN A1
 
+// Constants
 #define DEFAULT_SPEED     35902
 #define RANGE             200
-#define UPPER_BOUND       1024 / 2 + (RANGE / 2)
-#define LOWER_BOUND       1024 / 2 - (RANGE / 2)
+#define ANALOG_RESOLUTION 1024
+#define UPPER_BOUND       ANALOG_RESOLUTION / 2 + (RANGE / 2)
+#define LOWER_BOUND       ANALOG_RESOLUTION / 2 - (RANGE / 2)
 #define SPEED_MULTIPLIER  63
 
-// Initizalizieren des Displays. (I²C Adresse = 0x27, 16 Spalten, 2 Reihen)
+// Initialize the display. (I²C Adresse = 0x27, 16 Spalten, 2 Reihen)
 // See: https://funduino.de/nr-19-i%C2%B2c-display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -72,17 +56,16 @@ void setup()
   /* ************************ Setup des Timers (Ende) *********************** */
   /* MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM */
 
-
-
-  // Setze PULS_OUT_PIN als Ausgang
+  // Set PULS_OUT_PIN to output
   pinMode(PULS_OUT_PIN, OUTPUT);
 
-  // Setze DIRECTION_OUT_PIN als Ausgang
+  // Set DIRECTION_OUT_PIN to output
   pinMode(DIRECTION_OUT_PIN, OUTPUT);
+  
   // Setze Drehrichtung
   digitalWrite(DIRECTION_OUT_PIN, LOW);
-  Serial.begin(9600);
-  // kommunikation herstellen
+  
+  // Initialize the communication to the lcd display
   lcd.init();
   lcd.backlight();
 }
@@ -95,38 +78,46 @@ void loop()
   int potiValue = analogRead(SPEED_POTI_IN_PIN);
 
   if (potiValue > LOWER_BOUND && potiValue < UPPER_BOUND && potiValue != oldValue) {
+    // When the poti is in the middle section set the speed to the needed default speed to achieve the desiered speed of 1 rotation per minute
     ICR1 = DEFAULT_SPEED;
     rotationsPerDay = "1x";
     displayString = "STANDARD";
+
+    // Turn the backlight of the display of, to not interfere with the image
     lcd.noBacklight();
     lcd.clear();
   } else if(potiValue != oldValue) {
-    float currentSpeed = DEFAULT_SPEED - (abs(512 - potiValue) * SPEED_MULTIPLIER);
+    // When the poti is outside the middle section use the absolute distance from the center position to reduce the time until the next tick to the stepper motor is send, increasing its speed.
+    float currentSpeed = DEFAULT_SPEED - (abs((ANALOG_RESOLUTION / 2) - potiValue) * SPEED_MULTIPLIER);
+
+    // Calculate the rotations per day by dividing the current speed by the current speed and set the value for the timer to count to, to this value.
     rotationsPerDay = String(DEFAULT_SPEED / currentSpeed) + "x";
-    Serial.println(currentSpeed);
     ICR1 = currentSpeed;
+
+    // Turn the backlight on when adjusting the position of the camera
     lcd.backlight();
     lcd.clear();
   }
 
   if (potiValue < LOWER_BOUND && potiValue != oldValue) {
-    // Set Direction To High (Nicht der Default :D)
+    // Set direction to high, Reversing the direciton of the stepper motor
     digitalWrite(DIRECTION_OUT_PIN, HIGH);
     displayString = "RUECKLAUF";
   }
 
   if (potiValue > UPPER_BOUND && potiValue != oldValue) {
-    // Set Direction To Low (der Default :D)
+    // Set direction to high, Reversing the direction of the stepper motor
     digitalWrite(DIRECTION_OUT_PIN, LOW);
     displayString = "VORLAUF";
   }
 
-  // Home the curser
+  // Print the information to the display
   lcd.setCursor(0, 0);
   lcd.print("Speed: " + rotationsPerDay);
   lcd.setCursor(0, 1);
   lcd.print(displayString);
 
+  // Store the old value of the potentiometer, to check for changes
   oldValue = potiValue;
   delay(50);
 }
